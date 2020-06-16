0f9ce78bde0ef53bb43519f43e30ddef8acfc6b4
theluispestana <theluispestana@gmail.com>
2020-06-16 09:46:59 -0400

    added password column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_184204) do
+ActiveRecord::Schema.define(version: 2020_06_16_133639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2020_06_12_184204) do
     t.integer "group_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
+    t.string "password_confirmation"
   end
 
 end
